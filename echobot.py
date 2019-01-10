5ac8cd6307236da09c8c1300849df6b26ac316a0
Shadowedvaca <shadowedvaca@gmail.com>
2019-01-09 22:28:11 -0700

    v.04.1

--- echobot.py
+++ echobot.py
@@ -36,7 +36,7 @@ def get_last_chat_id_and_text(updates):
 
 
 def send_message(text, chat_id):
-    text = urllib.parse.quote_plus(text)
+    text = urllib.quote_plus(text)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
     
