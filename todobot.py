55f4b9553573d5e62c5a7e23d12af7fdef388283
Shadowedvaca <shadowedvaca@gmail.com>
2019-01-26 07:27:16 -0700

    tutorial done

--- todobot.py
+++ todobot.py
@@ -38,10 +38,18 @@ def get_last_chat_id_and_text(updates):
     return (text, chat_id)
 
 
-def send_message(text, chat_id):
-    text = urllib.quote_plus(text)
-    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
-    get_url(url)
+def build_keyboard(items):
+    keyboard = [[item] for item in items]
+    reply_markup = {"keyboard":keyboard, "one_time_keyboard": True}
+    return json.dumps(reply_markup)
+
+
+def send_message(text, chat_id, reply_markup=None):
+	text = urllib.quote_plus(text)
+	url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
+	if reply_markup:
+		url += "&reply_markup={}".format(reply_markup)
+	get_url(url)
     
 	
 def get_last_update_id(updates):
@@ -56,19 +64,28 @@ def handle_updates(updates):
 		try:
 			text = update["message"]["text"]
 			chat = update["message"]["chat"]["id"]
-			items = db.get_items()
-			if text in items:
-				db.delete_item(text)
-				items = db.get_items()
+			items = db.get_items(chat)
+			if text == "/done":
+				keyboard = build_keyboard(items)
+				send_message("Select an item to delete", chat, keyboard)
+			elif text == "/start":
+				send_message("Welcome to your personal To Do list. Send any text to me and I'll store it as an item. Send /done to remove items", chat)
+			elif text.startswith("/"):
+				continue
+			elif text in items:
+				db.delete_item(text, chat)
+				items = db.get_items(chat)
+				keyboard = build_keyboard(items)
+				send_message("Select an item to delete", chat, keyboard)
 			else:
-				db.add_item(text)
-				items = db.get_items()
-			message = "\n".join(items)
-			send_message(message, chat)
+				db.add_item(text, chat)
+				items = db.get_items(chat)
+				message = "\n".join(items)
+				send_message(message, chat)
 		except KeyError:
 			pass
-
-
+			
+	
 def main():
 	db.setup()
 	last_update_id = None
